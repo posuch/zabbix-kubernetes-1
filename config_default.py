13bcb5b0a6f0ed922e6c1d041904ff58e42740b9
Marc Schoechlin <marc.schoechlin@vico-research.com>
2019-12-23 11:21:06 +0100

    improve pod status monitoring

--- config_default.py
+++ config_default.py
@@ -4,6 +4,7 @@ verify_ssl = True
 debug = False
 zabbix_server = 'example.zabbix-server.com'
 zabbix_host = 'k8s-example-host'
+zabbix_debug = False
 
 discovery_interval_fast = 600
 data_interval_fast = 60 * 2
