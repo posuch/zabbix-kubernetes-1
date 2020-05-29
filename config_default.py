39fe8b9bb12b2e15a0928be86a47122bb83d2330
Marc Schoechlin <marc.schoechlin@vico-research.com>
2020-05-29 17:39:32 +0200

    rename

--- config_default.py
+++ config_default.py
@@ -20,9 +20,9 @@ web_api_token = ""
 web_api_cluster = 'k8s-test-cluster'
 
 discovery_interval_fast = 60 * 15
-data_interval_fast = 60 * 2
+resend_data_interval_fast = 60 * 2
 
 discovery_interval_slow = 60 * 60 *2
-data_interval_slow = 60 * 30
+resend_data_interval_slow = 60 * 30
 
 
